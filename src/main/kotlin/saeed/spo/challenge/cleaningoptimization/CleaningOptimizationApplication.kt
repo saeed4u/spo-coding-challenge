5e54ec9470f003795cc59e6e6684912ea0f0299c
= <=>
2019-07-02 19:56:49 +0300

    First push with bare repo

---
+++ src/main/kotlin/saeed/spo/challenge/cleaningoptimization/CleaningOptimizationApplication.kt
@@ -0,0 +1,11 @@
+package saeed.spo.challenge.cleaningoptimization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleaningOptimizationApplication
+
+fun main(args: Array<String>) {
+	runApplication<CleaningOptimizationApplication>(*args)
+}
---
+++ src/test/kotlin/saeed/spo/challenge/cleaningoptimization/CleaningOptimizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package saeed.spo.challenge.cleaningoptimization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleaningOptimizationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
